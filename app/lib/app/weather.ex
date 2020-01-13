e889c70267be10b9c9c355ee47afe04301330493
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-01-13 00:44:40 -0300

    Fix weather test

--- app/lib/app/weather.ex
+++ app/lib/app/weather.ex
@@ -6,7 +6,7 @@ defmodule App.Weather do
 
   def get_endpoint(location) do
     location = URI.encode(location)
-    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&APPID=#{get_appid()}"
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{get_appid()}"
   end
 
   def get_endpoint_by_zip_code(zip_code) do
